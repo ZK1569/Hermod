91838b5d3d9f627518f27584e6e5632cf34dc857
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-10-09 21:15:43 -0400

    ✨ new start message

--- hermod/src/main.rs
+++ hermod/src/main.rs
@@ -1,25 +1,11 @@
-use std::process;
-
 use env_logger::Env;
-use log::error;
-use models::server::Server;
+use utils::starter::start_message;
 
 mod models;
 mod utils;
 
 fn main() {
+    start_message();
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
-    let config = utils::config::Config::read();
-
-    let server = Server::new(config.port);
-
-    let listener_result = server.start_server();
-
-    let listener = match listener_result {
-        Ok(r) => r,
-        Err(err) => {
-            error!("Server failed to start ... \n{err}");
-            process::exit(1);
-        }
-    };
+    let _config = utils::config::Config::read();
 }
