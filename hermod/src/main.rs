a216d2839773b46e5880771116cba240f29cc47b
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-10-09 21:06:12 -0400

    🔧 update network lib into calice + new network files

--- hermod/src/main.rs
+++ hermod/src/main.rs
@@ -1,10 +1,25 @@
-use env_logger::Env;
+use std::process;
 
+use env_logger::Env;
+use log::error;
+use models::server::Server;
+
+mod models;
 mod utils;
 
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
     let config = utils::config::Config::read();
 
-    println!("{:?}", config)
+    let server = Server::new(config.port);
+
+    let listener_result = server.start_server();
+
+    let listener = match listener_result {
+        Ok(r) => r,
+        Err(err) => {
+            error!("Server failed to start ... \n{err}");
+            process::exit(1);
+        }
+    };
 }
---
+++ hermod/src/models/server.rs
@@ -0,0 +1,26 @@
+use std::{
+    io::Error,
+    net::{Shutdown, TcpListener, TcpStream},
+};
+
+use calice::models::network::Network;
+
+pub struct Server {
+    pub network: Network,
+}
+
+impl Server {
+    pub fn new(port: String) -> Server {
+        Server {
+            network: Network::new("127.0.0.1".to_owned(), port),
+        }
+    }
+
+    pub fn start_server(&self) -> Result<TcpListener, Error> {
+        TcpListener::bind(self.network.get_fulladdress())
+    }
+
+    pub fn close_connection(stream: &mut TcpStream) {
+        let _ = stream.shutdown(Shutdown::Both);
+    }
+}
