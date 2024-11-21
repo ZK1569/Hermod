8d0f91aaff353e4965796a14b7a73d0487348024
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-11-21 12:16:11 -0500

    🔧 no longer possible to enter an empty character string for user input

--- src/utils/input.rs
+++ src/utils/input.rs
@@ -1,12 +1,23 @@
 use std::io;
 
-pub fn input(message: &str) -> Result<String, io::Error> {
-    println!("{}", message);
+use log::warn;
 
+pub fn input(message: &str) -> Result<String, io::Error> {
     let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read password");
+
+    while input.len() <= 1 {
+        println!("{}", message);
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read password");
+
+        if input.len() <= 1 {
+            dbg!(input.len());
+            warn!("Please fill in the fields");
+            input = String::new();
+        }
+    }
 
     input.pop(); // INFO: Delete the '\n' at the end
 
